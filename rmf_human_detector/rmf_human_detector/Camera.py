5686fe2bcc67cbe9200e2a9370ab22718e683c9b
Yadunund <yadunund@openrobotics.org>
2022-05-10 19:49:07 +0800

    Added Camera class
    Signed-off-by: Yadunund <yadunund@openrobotics.org>

---
+++ rmf_human_detector/rmf_human_detector/Camera.py
@@ -0,0 +1,210 @@
+#!/usr/bin/env python3
+from pathlib import Path
+import sys
+import cv2
+import depthai as dai
+import blobconverter
+import numpy as np
+import time
+import threading
+
+class Detection:
+    def __init__(self,depth_x,depth_y,depth_z,width,height):
+        self.label = "person"
+        self.depth_x = depth_x
+        self.depth_y = depth_y
+        self.depth_z = depth_z
+        self.width = width
+        self.height = height
+'''
+Spatial detection network demo.
+    Performs inference on RGB camera and retrieves spatial location coordinates: x,y,z relative to the center of depth map.
+'''
+class Camera:
+    def __init__(self, visualize=True):
+        self.visualize = visualize
+        self.detections = [] # store detected persons as Detection objects
+        self.pipeline = self._create_pipeline()
+        self.quit = True
+        self.thread = threading.Thread(target=self._run)
+        self.thread.start()
+
+    def start(self):
+        print("Starting to detect humans...")
+        self.quit = False
+    
+    def stop(self):
+        self.quit = True
+
+    def __del__(self):
+        self.stop()
+        del self.pipeline
+        self.thread = None
+
+    def _create_pipeline(self):
+
+        nnBlobPath = blobconverter.from_zoo(name='mobilenet-ssd', shaves=6)
+        # MobilenetSSD label texts
+        self.labelMap = ["background", "aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat", "chair", "cow",
+                    "diningtable", "dog", "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor"]
+
+        self.syncNN = True
+
+        # Create pipeline
+        pipeline = dai.Pipeline()
+
+        # Add nodes for rgb, depth, and nn
+        camRgb = pipeline.create(dai.node.ColorCamera)
+        spatialDetectionNetwork = pipeline.create(dai.node.MobileNetSpatialDetectionNetwork)
+        monoLeft = pipeline.create(dai.node.MonoCamera)
+        monoRight = pipeline.create(dai.node.MonoCamera)
+        stereo = pipeline.create(dai.node.StereoDepth)
+
+        # Create connections
+        xoutRgb = pipeline.create(dai.node.XLinkOut)
+        xoutNN = pipeline.create(dai.node.XLinkOut)
+        xoutBoundingBoxDepthMapping = pipeline.create(dai.node.XLinkOut)
+        xoutDepth = pipeline.create(dai.node.XLinkOut)
+
+        # Create message streams
+        xoutRgb.setStreamName("rgb")
+        xoutNN.setStreamName("detections")
+        xoutBoundingBoxDepthMapping.setStreamName("boundingBoxDepthMapping")
+        xoutDepth.setStreamName("depth")
+
+        # Set properties
+        camRgb.setPreviewSize(300, 300)
+        camRgb.setResolution(dai.ColorCameraProperties.SensorResolution.THE_1080_P)
+        camRgb.setInterleaved(False)
+        camRgb.setColorOrder(dai.ColorCameraProperties.ColorOrder.BGR)
+
+        monoLeft.setResolution(dai.MonoCameraProperties.SensorResolution.THE_400_P)
+        monoLeft.setBoardSocket(dai.CameraBoardSocket.LEFT)
+        monoRight.setResolution(dai.MonoCameraProperties.SensorResolution.THE_400_P)
+        monoRight.setBoardSocket(dai.CameraBoardSocket.RIGHT)
+
+        # Setting node configs
+        stereo.initialConfig.setConfidenceThreshold(255)
+        stereo.setDefaultProfilePreset(dai.node.StereoDepth.PresetMode.HIGH_DENSITY)
+        # Align depth map to the perspective of RGB camera, on which inference is done
+        stereo.setDepthAlign(dai.CameraBoardSocket.RGB)
+        stereo.setOutputSize(monoLeft.getResolutionWidth(), monoLeft.getResolutionHeight())
+
+        spatialDetectionNetwork.setBlobPath(nnBlobPath)
+        spatialDetectionNetwork.setConfidenceThreshold(0.5)
+        spatialDetectionNetwork.input.setBlocking(False)
+        spatialDetectionNetwork.setBoundingBoxScaleFactor(1.0)
+        spatialDetectionNetwork.setDepthLowerThreshold(100)
+        spatialDetectionNetwork.setDepthUpperThreshold(5000)
+
+        # Link nodes
+        monoLeft.out.link(stereo.left)
+        monoRight.out.link(stereo.right)
+
+        camRgb.preview.link(spatialDetectionNetwork.input)
+        if self.syncNN:
+            spatialDetectionNetwork.passthrough.link(xoutRgb.input)
+        else:
+            camRgb.preview.link(xoutRgb.input)
+
+        spatialDetectionNetwork.out.link(xoutNN.input) # detections
+        spatialDetectionNetwork.boundingBoxMapping.link(xoutBoundingBoxDepthMapping.input)
+
+        stereo.depth.link(spatialDetectionNetwork.inputDepth)
+        spatialDetectionNetwork.passthroughDepth.link(xoutDepth.input)
+
+        return pipeline
+
+    def _run(self):
+        # Connect to device and start pipeline
+        with dai.Device(self.pipeline) as device:
+
+            # Output queues will be used to get the rgb frames and nn data from the outputs defined above
+            previewQueue = device.getOutputQueue(name="rgb", maxSize=4, blocking=False)
+            detectionNNQueue = device.getOutputQueue(name="detections", maxSize=4, blocking=False)
+            xoutBoundingBoxDepthMapping = device.getOutputQueue(name="boundingBoxDepthMapping", maxSize=4, blocking=False)
+            depthQueue = device.getOutputQueue(name="depth", maxSize=4, blocking=False)
+
+            while True:
+                if self.quit:
+                    time.sleep(1)
+                    continue
+
+                inDet = detectionNNQueue.get()
+                if inDet is None:
+                    continue
+
+                depth = depthQueue.get()
+                depthFrame = depth.getFrame() # depthFrame values are in millimeters
+
+                detections = inDet.detections
+                roiDatas = xoutBoundingBoxDepthMapping.get().getConfigData()
+                print(f"num detections : {len(detections)} num rois : {len(roiDatas)}")
+                # todo track objects and update on when there is significant change in position
+                self.detections = []
+
+                if (len(detections) != len(roiDatas)):
+                    print("oops")
+                    continue
+                for i in range(len(detections)):
+                    detection = detections[i]
+                    roi = roiDatas[i].roi.denormalize(depthFrame.shape[1], depthFrame.shape[0])
+                    if detection.label >= len(self.labelMap):
+                        continue
+                    label = self.labelMap[detection.label]
+
+                    if (self.visualize):
+                        frame = previewQueue.get().getCvFrame()
+                        depthFrameColor = cv2.normalize(depthFrame, None, 255, 0, cv2.NORM_INF, cv2.CV_8UC1)
+                        depthFrameColor = cv2.equalizeHist(depthFrameColor)
+                        depthFrameColor = cv2.applyColorMap(depthFrameColor, cv2.COLORMAP_HOT)
+                        topLeft = roi.topLeft()
+                        bottomRight = roi.bottomRight()
+                        xmin = int(topLeft.x)
+                        ymin = int(topLeft.y)
+                        xmax = int(bottomRight.x)
+                        ymax = int(bottomRight.y)
+                        cv2.rectangle(depthFrameColor, (xmin, ymin), (xmax, ymax), 255, cv2.FONT_HERSHEY_SCRIPT_SIMPLEX)
+
+                        frame_width = frame.shape[1]
+                        frame_height = frame.shape[0]
+                        x1 = int(detection.xmin * frame_width)
+                        x2 = int(detection.xmax * frame_width)
+                        y1 = int(detection.ymin * frame_height)
+                        y2 = int(detection.ymax * frame_height)
+
+                        cv2.putText(frame, str(label), (x1 + 10, y1 + 20), cv2.FONT_HERSHEY_TRIPLEX, 0.5, 255)
+                        cv2.putText(frame, "{:.2f}".format(detection.confidence*100), (x1 + 10, y1 + 35), cv2.FONT_HERSHEY_TRIPLEX, 0.5, 255)
+                        cv2.putText(frame, f"X: {int(detection.spatialCoordinates.x)} mm", (x1 + 10, y1 + 50), cv2.FONT_HERSHEY_TRIPLEX, 0.5, 255)
+                        cv2.putText(frame, f"Y: {int(detection.spatialCoordinates.y)} mm", (x1 + 10, y1 + 65), cv2.FONT_HERSHEY_TRIPLEX, 0.5, 255)
+                        cv2.putText(frame, f"Z: {int(detection.spatialCoordinates.z)} mm", (x1 + 10, y1 + 80), cv2.FONT_HERSHEY_TRIPLEX, 0.5, 255)
+
+                        cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 0), cv2.FONT_HERSHEY_SIMPLEX)
+
+                        cv2.imshow("depth", depthFrameColor)
+                        cv2.imshow("preview", frame)
+
+                    if label != "person":
+                        continue
+
+                    # topLeft = roi.topLeft()
+                    # bottomRight = roi.bottomRight()
+                    # xmin = int(topLeft.x)
+                    # ymin = int(topLeft.y)
+                    # xmax = int(bottomRight.x)
+                    # ymax = int(bottomRight.y)
+                    width = roi.size().width / 1000.0
+                    height = roi.size().height / 1000.0
+                    depth_x = detection.spatialCoordinates.x / 1000.0
+                    depth_y = detection.spatialCoordinates.y / 1000.0
+                    depth_z = detection.spatialCoordinates.z / 1000.0
+                    self.detections.append(Detection(depth_x,depth_y,depth_z,width,height))
+                    print(f"Detected person of height {height : .2f} and width {width : .2f}")
+
+
+                if cv2.waitKey(1) == ord('q'):
+                    break
+
+if __name__ == '__main__':
+    c = Camera()
+    c.start()
---
+++ rmf_human_detector/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+package_name = 'rmf_human_detector'
+
+setup(
+    name=package_name,
+    version='0.0.1',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Yadunund',
+    maintainer_email='yadunund@openrobotics.org',
+    description='Detect humans using depthai and publish to RMF',
+    license='Apache License 2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
